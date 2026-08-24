78c29120c0f14c9cf7cc3786e63919a9b5aac9dc
Luis Gallegos <luis@macbook.local>
2026-08-23 22:08:53 -0700

    done

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-num_1 = input()
-num_2 = input()
+num = int(input())
 
-str_to_num_1 = int(num_1)
-str_to_num_2 = int(num_2)
 
-print(str_to_num_1 + str_to_num_2)
+if num > 0:
+    print("positive")
+elif num < 0:
+    print("negative")
+else:
+    print("zero")
